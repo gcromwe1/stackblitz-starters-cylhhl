8f4c10b419b50b80612dc135edfd64cefab40a49
gcromwe1 <gcromwe1@tnstate.edu>
2024-11-23 17:18:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 fetch('mock.json')  
   .then(response => response.json())
   .then(students => {
-   
+    // Generate GPA for each student (between 2.6 and 4.0)
     students.forEach(student => {
-      student.gpa = (Math.random() * (4.0 - 2.6) + 2.6).toFixed(2); 
+      student.gpa = (Math.random() * (4.0 - 2.6) + 2.6).toFixed(2); // Generate random GPA between 2.6 and 4.0 and round to two decimals
     });
 
     const searchInput = document.getElementById('searchInput');
